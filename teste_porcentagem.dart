86c0b42973a62ace9f0d3677086e00e3561a1a41
icesousa <ice-l337elite@hotmail.com>
2023-04-11 19:55:25 -0300

    first commit

---
+++ teste_porcentagem.dart
@@ -0,0 +1,32 @@
+/*QUESTÃO:
+Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
+SP – R$67.836,43
+RJ – R$36.678,66
+MG – R$29.229,88
+ES – R$27.165,48
+Outros – R$19.849,53
+Escreva um programa na linguagem que desejar onde calcule o percentual de representação que cada estado 
+teve dentro do valor total mensal da distribuidora.
+
+*/
+
+
+
+
+
+void main() {
+  var faturamento = {
+    'SP': 67836.43,
+    'RJ': 36678.66,
+    'MG': 29229.88,
+    'ES': 27165.48,
+    'Outros': 19849.53,
+  };
+
+  double totalFaturamento = faturamento.values.fold(0, (a, b) => a + b);
+
+  faturamento.forEach((estado, valor) {
+    double percentual = (valor / totalFaturamento) * 100;
+    print('$estado: ${percentual.toStringAsFixed(2)}%');
+  });
+}
